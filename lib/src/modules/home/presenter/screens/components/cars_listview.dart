252904ba1a693c186cbc03706dd5ba26461763e3
Vinicius Oliveira <viniciusoliverrs@gmail.com>
2024-03-18 23:54:33 -0300

    test de widget do cars_listview

--- lib/src/modules/home/presenter/screens/components/cars_listview.dart
+++ lib/src/modules/home/presenter/screens/components/cars_listview.dart
@@ -13,86 +13,112 @@ class CarsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: cars.length,
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      separatorBuilder: (context, index) => const SizedBox(height: 10),
-      itemBuilder: (context, index) {
-        final car = cars[index];
-        return Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Colors.grey.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Visibility(
+      visible: cars.isNotEmpty,
+      replacement: const Center(
+        child: Text('No cars found'),
+      ),
+      child: ListView.separated(
+        key: const Key('cars_listview'),
+        itemCount: cars.length,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        separatorBuilder: (context, index) => const SizedBox(height: 10),
+        itemBuilder: (context, index) {
+          final car = cars[index];
+          return CarItemTile(
+            car: car,
+            onTap: onTap,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CarItemTile extends StatelessWidget {
+  const CarItemTile({
+    super.key,
+    required this.car,
+    required this.onTap,
+  });
+
+  final CarEntity car;
+  final Function(CarEntity p1) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
             children: [
-              Row(
+              Expanded(
+                child: Text(car.modelName),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: Text(car.modelName),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Portas: ',
+                      style: DefaultTextStyle.of(context).style,
+                      children: [
+                        TextSpan(
+                          text: car.doors.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text: 'Portas: ',
-                          style: DefaultTextStyle.of(context).style,
-                          children: [
-                            TextSpan(
-                              text: car.doors.toString(),
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
+                  RichText(
+                    text: TextSpan(
+                      text: 'Ano: ',
+                      style: DefaultTextStyle.of(context).style,
+                      children: [
+                        TextSpan(
+                          text: car.year.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      RichText(
-                        text: TextSpan(
-                          text: 'Ano: ',
-                          style: DefaultTextStyle.of(context).style,
-                          children: [
-                            TextSpan(
-                              text: car.year.toString(),
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
+                      ],
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Preço: ',
+                      style: DefaultTextStyle.of(context).style,
+                      children: [
+                        TextSpan(
+                          text: car.price.toString(),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      RichText(
-                        text: TextSpan(
-                          text: 'Preço: ',
-                          style: DefaultTextStyle.of(context).style,
-                          children: [
-                            TextSpan(
-                              text: car.price.toString(),
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () => onTap(car),
+                    child: Row(
+                      children: [
+                        Icon(
+                          car.isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: car.isFavorite ? Colors.red : Colors.grey,
+                          size: 30,
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      GestureDetector(
-                        onTap: () => onTap(car),
-                        child: Row(
-                          children: [
-                            Icon(
-                              car.isFavorite ? Icons.favorite : Icons.favorite_border,
-                              color: car.isFavorite ? Colors.red : Colors.grey,
-                              size: 30,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
             ],
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
---
+++ test/src/modules/home/presenter/screens/components/cars_listview_test.dart
@@ -0,0 +1,99 @@
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ws_work_teste_mobile/src/core/domain/entities/car.dart';
+import 'package:ws_work_teste_mobile/src/modules/home/presenter/screens/components/cars_listview.dart';
+
+void main() {
+  group('CarsListView widget', () {
+    testWidgets('should display a list of cars', (WidgetTester tester) async {
+      final cars = List.generate(
+          3,
+          (index) => CarEntity(
+                id: index + 1,
+                modelName: Faker().randomGenerator.string(6),
+                doors: Faker().randomGenerator.integer(4),
+                year: Faker().randomGenerator.integer(2022),
+                price: Faker().randomGenerator.decimal(),
+                isFavorite: Faker().randomGenerator.boolean(),
+                color: Faker().randomGenerator.string(6),
+                createdAt: DateTime.now(),
+                fuel: Faker().randomGenerator.string(6),
+                modelId: Faker().randomGenerator.integer(10),
+              ));
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CarsListView(
+              cars: cars,
+              onTap: (car) {},
+            ),
+          ),
+        ),
+      );
+      final listView = find.byKey(const Key('cars_listview'));
+
+      expect(listView, findsOneWidget);
+      expect(find.byType(CarItemTile), findsNWidgets(cars.length));
+    });
+
+    testWidgets('should call onTap when a car is tapped', (WidgetTester tester) async {
+      final cars = List.generate(
+          1,
+          (index) => CarEntity(
+                id: index,
+                modelName: Faker().randomGenerator.string(6),
+                doors: Faker().randomGenerator.integer(4),
+                year: Faker().randomGenerator.integer(2022),
+                price: Faker().randomGenerator.decimal(),
+                isFavorite: false,
+                color: Faker().randomGenerator.string(6),
+                createdAt: DateTime.now(),
+                fuel: Faker().randomGenerator.string(6),
+                modelId: Faker().randomGenerator.integer(10),
+              ));
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CarsListView(
+              cars: cars,
+              onTap: (car) {
+                final carIndex = cars.indexWhere((c) => c.id == car.id);
+                cars[carIndex] = car.copyWith(isFavorite: true);
+              },
+            ),
+          ),
+        ),
+      );
+
+      final listView = find.byKey(const Key('cars_listview'));
+      expect(listView, findsOneWidget);
+
+      final carItemTile = find.byType(GestureDetector).first;
+      await tester.tap(carItemTile);
+      await tester.pump();
+
+      expect(cars.first.isFavorite, true);
+    });
+
+    testWidgets('should display a message when the list is empty', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CarsListView(
+              cars: const [],
+              onTap: (car) {},
+            ),
+          ),
+        ),
+      );
+
+      final listView = find.byKey(const Key('cars_listview'));
+      expect(listView, findsNothing);
+
+      final emptyListMessage = find.text('No cars found');
+      expect(emptyListMessage, findsOneWidget);
+    });
+  });
+}
